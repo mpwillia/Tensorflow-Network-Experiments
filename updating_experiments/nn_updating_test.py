a34f368adc5b96c50b561044381e5b7db5f1b451
Michael Williams <mike@clwill.com>
2017-03-04 14:38:22 -0800

    added more test cases

--- updating_experiments/nn_updating_test.py
+++ updating_experiments/nn_updating_test.py
@@ -17,11 +17,24 @@ import tests
 
 def main():
     mnist_data = load_mnist()
-    mnist_net = load_mnist_network(True)
+    #mnist_net = load_mnist_network(False)
     
     #run_simple_baseline(mnist_data, mnist_net)
     #run_drip_baseline(mnist_data, mnist_net)
-    run_holdout_baseline(mnist_data, mnist_net)
+    #run_holdout_baseline(mnist_data, mnist_net)
+    #run_naive_holdout_baseline(mnist_data, mnist_net)
+
+
+    #run_holdout_test(mnist_data, 0.0)
+    #run_holdout_test(mnist_data, 0.00001)
+    run_holdout_test(mnist_data, 0.01)
+    #run_holdout_test(mnist_data, 0.05)
+    #run_holdout_test(mnist_data, 0.1)
+    #run_holdout_test(mnist_data, 0.25)
+    #run_holdout_test(mnist_data, 0.5)
+    #run_holdout_test(mnist_data, 0.75)
+    #run_holdout_test(mnist_data, 0.9)
+    #run_holdout_test(mnist_data, 0.99999)
     #run_drip_basic_test(mnist_data, mnist_net)
 
     #run_number_baseline(mnist, filter_labels = [2,5,6])
@@ -38,9 +51,20 @@ def run_holdout_baseline(data, net):
     print_fit_results(initial_results, '8.3%', 'Holdout Initial')
     print_fit_results(holdout_results, '8.3%', 'Holdout Final')
 
+def run_naive_holdout_baseline(data, net):
+    initial_results, holdout_results = baselines.naive_holdout.run(data, net)
+    print_fit_results(initial_results, '8.3%', 'Holdout Initial')
+    print_fit_results(holdout_results, '8.3%', 'Holdout Final')
+
 def run_drip_baseline(data, net):
     drip_results = baselines.drip.run(data, net)
     print_fit_results(drip_results, '8.3%', 'Drip Baseline')
+
+def run_holdout_test(data, recall_p):
+    initial_results, holdout_results = tests.holdout.run(data, recall_p)
+    print("Holdout Test Results with Recall of {:7.2%}".format(recall_p))
+    print_fit_results(initial_results, '8.3%', 'Holdout Initial')
+    print_fit_results(holdout_results, '8.3%', 'Holdout Final')
 
 def run_drip_basic_test(data, net):
     drip_results = tests.basic_drip.run(data, net)
